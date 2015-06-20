a2a06d6b25fecf76941d18ebe77bf47e372fdb11
William Bruno <rocha_bruno@hotmail.com>
2015-06-20 11:25:51 -0300

    More test case

--- capitulo_7/capitulo_7.1/tests/util_test.js
+++ capitulo_7/capitulo_7.1/tests/util_test.js
@@ -22,4 +22,27 @@ suite('arraySum', function() {
     var sum = util.arraySum([-1,-2]);
     assert.equal(sum, -3);
   });
+  test('should sum the array [0,undefined]', function() {
+    var sum = util.arraySum([0,undefined]);
+    assert.equal(sum, 0);
+  });
+});
+
+suite('guid', function() {
+  test('should have a valid format', function() {
+    var uuid = util.guid();
+    console.log(uuid);
+    assert.ok(/^[a-z|\d]{8}-[a-z|\d]{4}-[a-z|\d]{4}-[a-z|\d]{4}-[a-z|\d]{12}$/.test(uuid));
+  });
+  test('should generate uniques uuids', function() {
+    var uuid1 = util.guid();
+    var uuid2 = util.guid();
+    var uuid3 = util.guid();
+    var uuid4 = util.guid();
+
+    assert.notEqual(uuid1, uuid2);
+    assert.notEqual(uuid2, uuid3);
+    assert.notEqual(uuid3, uuid4);
+    assert.notEqual(uuid1, uuid4);
+  });
 });
--- capitulo_7/capitulo_7.1/util.js
+++ capitulo_7/capitulo_7.1/util.js
@@ -4,10 +4,21 @@
 
 var arraySum = function (arr) {
   return arr.reduce(function(prev, curr) {
-    return prev + curr;
+    return prev + (isNaN(curr) ? 0 : curr);
   });
 };
 
+var guid = function() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+};
+
 module.exports = {
-  arraySum: arraySum
+  arraySum: arraySum,
+  guid: guid
 };
