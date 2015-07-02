5b3e14677e9be92cb17e46228420c1ccb436d7bc
William Bruno <rocha_bruno@hotmail.com>
2015-07-01 22:53:00 -0300

    Fix assync test

--- capitulo_7/capitulo_7.3/tests/unit/controllers/StormtropperController.js
+++ capitulo_7/capitulo_7.3/tests/unit/controllers/StormtropperController.js
@@ -2,42 +2,47 @@ var fixtures                = require('../../fixtures'),
     StormtropperModel       = require('../../../models/StormtropperModel')(fixtures.mongo),
     StormtropperController  = new require('../../../controllers/StormtropperController')(StormtropperModel),
     assert                  = require('assert'),
-    debug                   = require('debug')('Stormtroppers:test');
+    debug                   = require('debug')('livro_nodejs:test');
 
 var request = fixtures.request;
 var response = fixtures.response;
 request.params._id = '5569c7fe17fa3690d24de049';
 
 describe('StormtropperController', function () {
-  it('#create', function() {
+  it('#create', function(done) {
     request.body.name = 'Rex';
     response.json = function(obj) {
       assert.deepEqual(obj, {_id: '5569c7fe17fa3690d24de049', nickname: 'Rex'});
+      done();
     };
     StormtropperController.create(request, response, fixtures.next);
   });
-  it('#getAll', function() {
+  it('#getAll', function(done) {
     response.json = function(obj) {
       debug(obj);
       assert.deepEqual(obj, [{nickname: 'Fives'}, {nickname: 'Fox'}]);
+      done();
     };
     StormtropperController.getAll(request, response, fixtures.next);
   });
-  it('#getById', function() {
+  it('#getById', function(done) {
     response.json = function(obj) {
       assert.deepEqual(obj, {nickname: 'Fives'});
+      done();
     };
     StormtropperController.getById(request, response, fixtures.next);
   });
-  it('#update', function() {
+  it('#update', function(done) {
     response.json = function(obj) {
       assert.deepEqual(obj, {ok: 1, nModified: 1, n: 1 });
+      done();
     };
     StormtropperController.update(request, response, fixtures.next);
   });
-  it('#delete', function() {
+  it('#delete', function(done) {
     response.json = function(obj) {
       assert.deepEqual(obj, {ok: 1, n: 1});
+      done();
     };
     StormtropperController.remove(request, response, fixtures.next);
   });
