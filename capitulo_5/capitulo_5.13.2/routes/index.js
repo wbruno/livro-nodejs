0c2d9293861a8b237dda2571aebccc875ba658b5
William Bruno <rocha_bruno@hotmail.com>
2015-07-17 22:36:18 -0300

    Chapter 5.13.2

---
+++ capitulo_5/capitulo_5.13.2/routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express'),
+    jwt     = require('jwt-simple'),
+    moment  = require('moment'),
+    config  = require('config'),
+    router = express.Router();
+
+router.get('/', function (request, response) {
+  response.status(201);
+  response.json({ 'name': 'William Bruno', 'email': 'wbrunom@gmail.com' });
+});
+
+router.post('/login', function (request, response, next) {
+  var username = request.body.username;
+  var password = request.body.password;
+
+  if(username === 'rebels' && password === '1138') {
+    var expires = moment().add(7, 'days').valueOf();
+    var token = jwt.encode({
+      user: username,
+      exp: expires
+    }, config.get('jwtTokenSecret'));
+
+    response.json({
+      token: token
+    });
+  } else {
+    var err = new Error('Unauthorized');
+    err.status = 401;
+    next(err);
+  }
+});
+// stormtroppers
+router.use('/stormtroppers', require('./stormtroppers'));
+
+module.exports = router;
