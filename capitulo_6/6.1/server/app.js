1974784405b2e87a795c518ec3b96a3d5b328bd2
William Bruno <wbrunom@gmail.com>
2021-01-04 09:49:59 -0300

    add: mongodb.json

--- capitulo_6/6.1/server/app.js
+++ capitulo_6/6.1/server/app.js
@@ -2,7 +2,6 @@ import express from 'express'
 import path from 'path'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
-import nunjucks from 'nunjucks'
 const app = express()
 app.get('/', (request, response) => response.send('Always pass on what you have learned.'))
 const __dirname = path.join(dirname(fileURLToPath(import.meta.url)), '../');
