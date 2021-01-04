326d70b763482a8dd459832a478980b7ad62aac6
William Bruno <wbrunom@gmail.com>
2021-01-04 17:14:30 -0300

    add: use effect

--- capitulo_6/6.3.4/src/App.js
+++ capitulo_6/6.3.4/src/App.js
@@ -1,63 +1,43 @@
 import './App.css'
-import { Component } from 'react'
-class THead extends Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    return (
-      <thead>
-        <tr> 
-          { this.props.items.map((item,i) => <th key={i}>{item}</th>) }
-        </tr>
-      </thead>
-    )
-  }
-}
-class TBody extends Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    return (
-      <tbody>
-      {
-        this.props.items.map((item,i) => {
-          return <TLine key={i} item={item} />
-        })
-      }
-      </tbody>
-    )
-  }
-}
-class TLine extends Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    return (
-      <tr>
-        <td>{this.props.item._id}</td>
-        <td>{this.props.item.name}</td>
-        <td>{this.props.item.patent}</td>
+import { useState, useEffect } from 'react'
+function THead(props) {
+  return (
+    <thead>
+      <tr> 
+        { props.items.map((item,i) => <th key={i}>{item}</th>) }
       </tr>
-    )
-  }
+    </thead>
+  )
+}
+function TBody(props) {
+  return (
+    <tbody>
+    {
+      props.items.map(item => {
+        return <TLine key={item._id} item={item} />
+      })
+    }
+    </tbody>
+  )
+}
+function TLine(props) {
+  return (
+    <tr>
+      <td>{props.item._id}</td>
+      <td>{props.item.name}</td>
+      <td>{props.item.patent}</td>
+    </tr>
+  )
 }
 function App() {
-  const troopers = [{
-      "_id": "5ff30c2e7952ec31de6b8e1a",
-      "name": "CT-27-5555",
-      "nickname": "Fives",
-      "patent": "Soldier"
-    },
-    {
-      "_id": "5ff30c2e7952ec31de6b8e18",
-      "name": "CC-2224",
-      "nickname": "Cody",
-      "patent": "Commander"
-    }
-  ]
+  const [troopers, setTroopers] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/troopers')
+        .then(response => response.json())
+        .then(data => setTroopers(data))
+  }, [])
+
   return (
     <>
       <h1>Stormtroopers</h1>
