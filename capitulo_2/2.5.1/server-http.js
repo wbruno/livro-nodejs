bb4536dfc99453faf462863df6dff66fd3127441
William Bruno <wbrunom@gmail.com>
2023-05-10 22:16:44 -0300

    feat: add chapter 2.5.1

---
+++ capitulo_2/2.5.1/server-http.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const routes = new Map()
+routes.set('/', (request, response) => response.end('Open the blast doors!\n'))
+routes.set('/close', (request, response) => response.end('Open the blast doors!\n'))
+const phrases = [
+  'RRRAARRWHHGWWR',
+  'RWGWGWARAHHHHWWRGGWRWRW',
+  'WWWRRRRRRGWWWRRRR'
+]
+routes.set('/chewbacca', (request, response) => {
+  const randomIndex = Math.ceil(Math.random() * phrases.length) -1
+  const say = phrases[randomIndex]
+  response.end(`${say}\n`)
+})
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    if (!routes.has(request.url))
+      return response.end('No doors!\n')
+
+    return routes.get(request.url)(request, response)
+})
+server.listen(1337, '127.0.0.1', () => {
+  console.log('Server running at http://127.0.0.1:1337/')
+})
